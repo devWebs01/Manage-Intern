067282ba63a1d2cbe03b43d0f7fb2f23e6a51a1e
Adi Putra <m.a.syaputra.94@gmail.com>
2025-02-19 17:00:58 +0700

    set layout template

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -8,4 +8,10 @@ class Home extends BaseController
     {
         return view('welcome_message');
     }
+
+    public function dashboard()
+    {
+        // return 'dashboard page';
+        return view('dashboard');
+    }
 }
