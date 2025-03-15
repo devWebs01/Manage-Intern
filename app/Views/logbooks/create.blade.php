6dd88d57f57693fc662ada4be12f64fdbe6e974c
Adi Putra <m.a.syaputra.94@gmail.com>
2025-03-15 16:07:02 +0700

    add: test certificate

--- app/Views/logbooks/create.blade.php
+++ app/Views/logbooks/create.blade.php
@@ -1,8 +1,9 @@
-@extends('components.layout')
+@extends("components.layout")
 
-@section('header')
+
+@section("header")
     <li class="breadcrumb-item">
-        <a href="{{ site_url('logbooks') }}">
+        <a href="{{ site_url("logbooks") }}">
             Logbook
         </a>
     </li>
@@ -11,40 +12,45 @@
             Logbook Baru
         </a>
     </li>
-@endsection
-
-@section('content')
+    @endsection
+    
+    @section("content")
+    @include("layouts.editor")
     <div class="card rounded">
         <div class="card-body">
-            <form action="{{ site_url('logbooks') }}" method="post">
+            <form action="{{ site_url("logbooks") }}" method="post">
                 <div class="d-none">
                     {{ csrf_field() }}
                     <!-- Contoh: Mengambil participant_id dari fungsi helper user() -->
-                    <input type="hidden" name="participant_id" id="participant_id" value="{{ user()->id ?? '' }}" readonly>
+                    <input type="hidden" name="participant_id" id="participant_id" value="{{ user()->id ?? "" }}" readonly>
                 </div>
                 <div class="row">
                     <div class="col-12 mb-3">
                         <label for="date" class="form-label">Tanggal</label>
-                        <input type="date" class="form-control
-                        @error('date')
+                        <input type="date"
+                            class="form-control
+                        @error("date")
                         is-invalid
                         @enderror
-                        " name="date" id="date" value="{{ old('date') }}">
-                         @error('date')
+                        "
+                            name="date" id="date" value="{{ old("date") }}">
+                        @error("date")
                             <div class="invalid-feedback">
-                                {{ session('errors')['date'] }}
+                                {{ session("errors")["date"] }}
                             </div>
                         @enderror
                     </div>
                     <div class="col-12 mb-3">
                         <label for="activity" class="form-label">Isi Logbook</label>
-                        <textarea class="form-control 
-                        @error('activity')
+                        <textarea id="editor"
+                            class="form-control 
+                        @error("activity")
                         is-invalid
-                        @enderror" name="activity" id="activity" rows="10">{{ old('activity') }}</textarea>
-                         @error('activity')
+                        @enderror"
+                            name="activity" id="activity" rows="10">{{ old("activity") }}</textarea>
+                        @error("activity")
                             <div class="invalid-feedback">
-                                {{ session('errors')['activity'] }}
+                                {{ session("errors")["activity"] }}
                             </div>
                         @enderror
                     </div>
