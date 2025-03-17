004ae9ab0be615b3d08f4d133f15e0f8dd709fff
Adi Putra <m.a.syaputra.94@gmail.com>
2025-03-17 23:30:20 +0700

    fix: menu admin, mentor, peserta

--- app/Views/components/sidebar.blade.php
+++ app/Views/components/sidebar.blade.php
@@ -24,116 +24,118 @@
                     </a>
                 </li>
 
-                <li class="pc-item pc-caption">
-                    <label>Menu Admin</label>
-                    <i class="ti ti-dashboard">
-                    </i>
-                </li>
-                <li class="pc-item">
-                    <a href="{{ site_url("users") }}" class="pc-link">
-                        <span class="pc-micon">
-                            <i class="ti ti-typography">
-                            </i>
-                        </span>
-                        <span class="pc-mtext">Admin</span>
-                    </a>
-                </li>
-                <li class="pc-item">
-                    <a href="{{ site_url("mentors") }}" class="pc-link">
-                        <span class="pc-micon">
-                            <i class="ti ti-typography">
-                            </i>
-                        </span>
-                        <span class="pc-mtext">Pembimbing</span>
-                    </a>
-                </li>
-                <li class="pc-item">
-                    <a href="{{ site_url("participants") }}" class="pc-link">
-                        <span class="pc-micon">
-                            <i class="ti ti-color-swatch">
-                            </i>
-                        </span>
-                        <span class="pc-mtext">Peserta</span>
-                    </a>
-                </li>
+                @if (User()->role === "ADMIN")
+                    <li class="pc-item pc-caption">
+                        <label>Menu Admin</label>
+                        <i class="ti ti-dashboard">
+                        </i>
+                    </li>
+                    <li class="pc-item">
+                        <a href="{{ site_url("users") }}" class="pc-link">
+                            <span class="pc-micon">
+                                <i class="ti ti-typography">
+                                </i>
+                            </span>
+                            <span class="pc-mtext">Admin</span>
+                        </a>
+                    </li>
 
-                <li class="pc-item pc-hasmenu">
-                    <a href="#!" class="pc-link">
-                        <span class="pc-micon">
-                            <i class="ti ti-menu">
-                            </i>
-                        </span>
-                        <span class="pc-mtext">Magang</span>
-                        <span class="pc-arrow">
-                            <i data-feather="chevron-right">
-                            </i>
-                        </span>
-                    </a>
-                    <ul class="pc-submenu">
-                        <li class="pc-item">
-                            <a class="pc-link" href="{{ site_url("internships") }}">Peserta Magang</a>
-                        </li>
-                        <li class="pc-item">
-                            <a class="pc-link" href="{{ site_url("assessment-indicators") }}">Indikator Penilaian</a>
-                        </li>
-                        <li class="pc-item">
-                            <a class="pc-link" href="{{ site_url("company-profile/show") }}">Profil Perusahaan</a>
-                        </li>
-                    </ul>
-                </li>
+                    <li class="pc-item">
+                        <a href="{{ site_url("mentors") }}" class="pc-link">
+                            <span class="pc-micon">
+                                <i class="ti ti-typography">
+                                </i>
+                            </span>
+                            <span class="pc-mtext">Pembimbing</span>
+                        </a>
+                    </li>
+                    <li class="pc-item">
+                        <a href="{{ site_url("participants") }}" class="pc-link">
+                            <span class="pc-micon">
+                                <i class="ti ti-color-swatch">
+                                </i>
+                            </span>
+                            <span class="pc-mtext">Peserta</span>
+                        </a>
+                    </li>
 
-                <li class="pc-item pc-caption">
-                    <label>Menu Pembimbing</label>
-                    <i class="ti ti-news">
-                    </i>
-                </li>
+                    <li class="pc-item pc-hasmenu">
+                        <a href="#!" class="pc-link">
+                            <span class="pc-micon">
+                                <i class="ti ti-menu">
+                                </i>
+                            </span>
+                            <span class="pc-mtext">Magang</span>
+                            <span class="pc-arrow">
+                                <i data-feather="chevron-right">
+                                </i>
+                            </span>
+                        </a>
+                        <ul class="pc-submenu">
+                            <li class="pc-item">
+                                <a class="pc-link" href="{{ site_url("internships") }}">Peserta Magang</a>
+                            </li>
+                            <li class="pc-item">
+                                <a class="pc-link" href="{{ site_url("assessment-indicators") }}">Indikator
+                                    Penilaian</a>
+                            </li>
+                            <li class="pc-item">
+                                <a class="pc-link" href="{{ site_url("company-profile/show") }}">Profil Perusahaan</a>
+                            </li>
+                        </ul>
+                    </li>
+                @elseif (User()->role === "MENTOR")
+                    <li class="pc-item pc-caption">
+                        <label>Menu Pembimbing</label>
+                        <i class="ti ti-news">
+                        </i>
+                    </li>
 
-                <li class="pc-item">
-                    <a href="{{ site_url("internships") }}" class="pc-link">
-                        <span class="pc-micon">
-                            <i class="ti ti-menu">
-                            </i>
-                        </span>
-                        <span class="pc-mtext">Peserta Magang</span>
-                    </a>
-                </li>
+                    <li class="pc-item">
+                        <a href="{{ site_url("internships") }}" class="pc-link">
+                            <span class="pc-micon">
+                                <i class="ti ti-menu">
+                                </i>
+                            </span>
+                            <span class="pc-mtext">Peserta Magang</span>
+                        </a>
+                    </li>
 
-                <li class="pc-item">
-                    <a href="{{ site_url("participant-assessments") }}" class="pc-link">
-                        <span class="pc-micon">
-                            <i class="ti ti-menu">
-                            </i>
-                        </span>
-                        <span class="pc-mtext">Penilaian Peserta</span>
-                    </a>
-                </li>
+                    <li class="pc-item">
+                        <a href="{{ site_url("participant-assessments") }}" class="pc-link">
+                            <span class="pc-micon">
+                                <i class="ti ti-menu">
+                                </i>
+                            </span>
+                            <span class="pc-mtext">Penilaian Peserta</span>
+                        </a>
+                    </li>
+                @elseif (User()->role === "PARTICIPANT")
+                    <li class="pc-item pc-caption">
+                        <label>Menu Peserta</label>
+                        <i class="ti ti-dashboard">
+                        </i>
 
-                <li class="pc-item pc-caption">
-                    <label>Menu Peserta</label>
-                    <i class="ti ti-dashboard">
-                    </i>
+                    <li class="pc-item">
+                        <a href="{{ site_url("presences") }}" class="pc-link">
+                            <span class="pc-micon">
+                                <i class="ti ti-lock">
+                                </i>
+                            </span>
+                            <span class="pc-mtext">Absen</span>
+                        </a>
+                    </li>
 
-                <li class="pc-item">
-                    <a href="{{ site_url("presences") }}" class="pc-link">
-                        <span class="pc-micon">
-                            <i class="ti ti-lock">
-                            </i>
-                        </span>
-                        <span class="pc-mtext">Absen</span>
-                    </a>
-                </li>
-
-                <li class="pc-item">
-                    <a href="{{ site_url("logbooks") }}" class="pc-link">
-                        <span class="pc-micon">
-                            <i class="ti ti-plant-2">
-                            </i>
-                        </span>
-                        <span class="pc-mtext">Logbook</span>
-                    </a>
-                </li>
-
-                </li>
+                    <li class="pc-item">
+                        <a href="{{ site_url("logbooks") }}" class="pc-link">
+                            <span class="pc-micon">
+                                <i class="ti ti-plant-2">
+                                </i>
+                            </span>
+                            <span class="pc-mtext">Logbook</span>
+                        </a>
+                    </li>
+                @endif
 
             </ul>
 
